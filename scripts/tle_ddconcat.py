f684fb745d697fbe96a37f2a18eae8518b708896
DAVID DEBOER <ddeboer@berkeley.edu>
2026-03-06 12:03:19 -0800

    fix output filename

--- scripts/tle_ddconcat.py
+++ scripts/tle_ddconcat.py
@@ -4,7 +4,7 @@ import argparse
 
 ap = argparse.ArgumentParser(description="Concatenate TLE .npz files into a single file.")
 ap.add_argument("-s", "--starter", help="Path to an existing .npz file to use as a starting point for concatenation, if any.", default={})
-ap.add_argument("-o", "--output", help="Path to output .npz filename. Default is T{{E1}}_{{E2}}.npz located in directory with the data", default=None)
+ap.add_argument("-o", "--output", help="Path to output .npz filename. Default is T{YYMMDD}_{YYMMDD}.npz located in directory with the data", default=None)
 ap.add_argument("-d", "--directory", help="Directory to use for tle file search (and output if default).", default="./tle")
 ap.add_argument("-g", "--glob", help="Glob pattern to match .npz files for concatenation.  Defaults to 'tle*.npz'.", default="tle*.npz")
 ap.add_argument("-c", "--cleanup", help="If set, original .npz files will be deleted after concatenation. Use with caution!", action="store_true")
