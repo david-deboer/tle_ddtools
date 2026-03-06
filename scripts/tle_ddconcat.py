97381c6c5f8abed6f42e3a4c1c4f76f97ff2c78b
DAVID DEBOER <ddeboer@berkeley.edu>
2026-03-06 12:30:50 -0800

    make cleanup flag more explicit

--- scripts/tle_ddconcat.py
+++ scripts/tle_ddconcat.py
@@ -7,7 +7,7 @@ ap.add_argument("-s", "--starter", help="Path to an existing .npz file to use as
 ap.add_argument("-o", "--output", help="Path to output .npz filename. Default is T{YYMMDD}_{YYMMDD}.npz located in directory with the data", default=None)
 ap.add_argument("-d", "--directory", help="Directory to use for tle file search (and output if default).", default="./tle")
 ap.add_argument("-g", "--glob", help="Glob pattern to match .npz files for concatenation.  Defaults to 'tle*.npz'.", default="tle*.npz")
-ap.add_argument("-c", "--cleanup", help="If set, original .npz files will be deleted after concatenation. Use with caution!", action="store_true")
+ap.add_argument("--cleanup", help="If set, original .npz files will be deleted after concatenation. Use with caution!", action="store_true")
 args = ap.parse_args()
 
 concatz(starter=args.starter, output_file=args.output, base_dir=args.directory, globster=args.glob, cleanup=args.cleanup)
