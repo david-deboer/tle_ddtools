f1410355597b222989352f935ee08380023ed7c7
DAVID DEBOER <ddeboer@berkeley.edu>
2026-03-06 11:48:29 -0800

    draft of ddconcat script

--- scripts/tle_ddconcat.py
+++ scripts/tle_ddconcat.py
@@ -1,1 +1,12 @@
 from tle_ddtools import tle_utils
+import argparse
+
+ap = argparse.ArgumentParser(description="Concatenate TLE .npz files into a single file.")
+ap.add_argument("-s", "--starter", help="Path to an existing .npz file to use as a starting point for concatenation, if any.", default={})
+ap.add_argument("-o", "--output", help="Output .npz filename. Default is T{{E1}}_{{E2}}.npz", default=None)
+ap.add_argument("-d", "--directory", help="Directory to use for ALL files.", default="./tle")
+ap.add_argument("-g", "--glob", help="Glob pattern to match .npz files for concatenation.  Defaults to 'tle*.npz'.", default="tle*.npz")
+ap.add_argument("-c", "--cleanup", help="If set, original .npz files will be deleted after concatenation. Use with caution!", action="store_true")
+args = ap.parse_args()
+
+tle_utils.concatz(starter=args.starter, output_file=args.output, base_dir=args.directory, globster=args.glob, cleanup=args.cleanup)
